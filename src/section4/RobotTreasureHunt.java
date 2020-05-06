551ed897d240d4071da5676ede8ecfc12985439e
root <root@1154a0b86fd2>
2020-05-06 02:56:17 +0000

    Robot Treasure Hunt

--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -1,4 +1,4 @@
-package section4;
+ package section4;
 
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
@@ -16,43 +16,65 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+		Robot Rob = new Robot ("mini");
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+			Rob.setAngle(0);
+			Rob.microMove(10);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+			Rob.setAngle(180);
+			Rob.microMove(10);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+				Rob.setAngle(-90);
+				Rob.microMove(10);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+			Rob.setAngle(90);
+			Rob.microMove(10);
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = Rob.getX();
+		int robotYLocation = Rob.getY();
+		
+			
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
+			System.out.println(robotXLocation+" "+ robotYLocation);
+		
 		
 		// 7. If robot is at same location as the little girl
+			
 		//      --make a pop-up tell the robot where to go next
+		if (robotXLocation==730&&robotYLocation==430) {
+			JOptionPane.showMessageDialog(null, "Go to boy");
+		}
 		
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
-		
+		if (robotXLocation==600&&robotYLocation==430) {
+			JOptionPane.showMessageDialog(null, "Keep going");
+			
+		}
 		// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
+		if (robotXLocation==200&&robotYLocation==430) {
+			treasureFound();
+		}
+	}
+
+	private void treasureFound(int i) {
+		// TODO Auto-generated method stub
 		
 	}
 
@@ -100,7 +122,7 @@ public class RobotTreasureHunt implements KeyEventDispatcher{
 	static void treasureFound() {
 		try {
 			URI uri = new URI("https://www.youtube.com/watch?v=G0aIg4N6aro");
-			java.awt.Desktop.getDesktop().browse(uri);
+			JOptionPane.showMessageDialog(null,"Treasure found!!!!!!" );
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
